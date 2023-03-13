d9fd88b7eeadb80ca70c6fc2e729e33a5c11e065
jubin369 <jubinayoob369@gmail.com>
2023-03-13 22:03:45 +0530

    adds launch date display page...

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SpaceX from "./components/SpaceX";
+import LaunchDate from "./components/LaunchDate";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<SpaceX />} />
+        <Route path="/launchdate/:id" element={<LaunchDate />} />
       </Routes>
     </>
   );
---
+++ spacex/src/components/LaunchDate.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { intervalToDuration } from "date-fns";
+import data from "../data/launchData";
+
+const LaunchDate = () => {
+  const { id } = useParams();
+  const launchData = data.launchDates.find((a) => a.Mission === id);
+  const [displayDate, setDisplayDate] = useState(null);
+
+  useEffect(() => {
+    let date = intervalToDuration({
+      start: new Date(),
+      end: new Date(launchData.Date),
+    });
+    setDisplayDate(date);
+  }, [displayDate]);
+
+  if (!launchData) {
+    return (
+      <Stack>
+        <Text>Launch date Not Found</Text>
+      </Stack>
+    );
+  }
+  return (
+    <Stack align="center" w="100%">
+      <Text textAlign="center" fontSize="larger" fontWeight="bold" p="30px">
+        Upcoming:{launchData?.Mission}
+      </Text>
+      <Stack
+        w="50%"
+        m="auto"
+        backgroundColor="blue.700"
+        color="white"
+        p="20px"
+        textAlign="center"
+      >
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.years}
+          </Text>
+        </Stack>
+        <Stack pb="20px">
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Years
+          </Text>
+        </Stack>
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.months}
+          </Text>
+        </Stack>
+        <Stack pb="20px">
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Months
+          </Text>
+        </Stack>
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.days}
+          </Text>
+        </Stack>
+        <Stack pb="20px">
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Dates
+          </Text>
+        </Stack>
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.hours}
+          </Text>
+        </Stack>
+        <Stack pb="20px">
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Hours
+          </Text>
+        </Stack>
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.minutes}
+          </Text>
+        </Stack>
+        <Stack>
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Minutes
+          </Text>
+        </Stack>
+        <Stack>
+          <Text fontWeight="bold" w="80px" m="auto">
+            {displayDate?.seconds}
+          </Text>
+        </Stack>
+        <Stack>
+          <Text border="1px" fontWeight="bold" p="5px" w="80px" m="auto">
+            Seconds
+          </Text>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default LaunchDate;
